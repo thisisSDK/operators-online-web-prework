9915cf70323069cb6b0be021770c8f078bee67c9
sdk1270 <sdk1270@gmail.com>
2018-10-16 22:05:24 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,23 @@
 def unsafe?(speed)
-
+  if !(speed > 60 || speed < 40)
+    false
+  else
+    true
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
 	
 
 
+#def unsafe?(speed)
+ # if speed > 60 || speed > 40
+  #  "true"
+  #else
+   # "false"
+#  end
+#end
